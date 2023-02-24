8a58ba42d982d8f6692434b8df45935f3066e31e
Woomymy <woomy@woomy.be>
2023-02-24 14:29:59 +0100

    vendor/extra: Rename LineageUpdaterOverlay to LineageUpdaterOverlayMT6781 and only include it in viva builds
    Signed-off-by: Woomymy <woomy@woomy.be>

--- product.mk
+++ product.mk
@@ -1,5 +1,8 @@
+# Only include MT6781 Updater overlay in viva builds
+ifneq ($(filter viva,$(subst lineage_,,$(TARGET_PRODUCT))),)
 PRODUCT_PACKAGES += \
-    LineageUpdaterOverlay
+    LineageUpdaterOverlayMT6781
+endif
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/extra
